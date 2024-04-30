58b5611671b621e5e5b10b9f09793664812f1402
찰흙고양이 <claaycaat@gmail.com>
2024-04-30 16:46:33 +0900

    initial commit

---
+++ src/main/kotlin/com/tiketeer/TiketeerWaiting/TiketeerWaitingApplication.kt
@@ -0,0 +1,11 @@
+package com.tiketeer.TiketeerWaiting
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TiketeerWaitingApplication
+
+fun main(args: Array<String>) {
+	runApplication<TiketeerWaitingApplication>(*args)
+}
---
+++ src/test/kotlin/com/tiketeer/TiketeerWaiting/TiketeerWaitingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tiketeer.TiketeerWaiting
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TiketeerWaitingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
