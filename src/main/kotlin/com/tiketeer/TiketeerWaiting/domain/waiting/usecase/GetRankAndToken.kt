4fe61a93ac62281510c19488cef865954e846f0b
punkryn <gns816715@gmail.com>
2024-05-02 14:41:08 +0900

    getrankandtoken usecase interface 작성

--- src/main/kotlin/com/tiketeer/TiketeerWaiting/domain/waiting/usecase/GetRankAndToken.kt
+++ src/main/kotlin/com/tiketeer/TiketeerWaiting/domain/waiting/usecase/GetRankAndToken.kt
@@ -3,9 +3,6 @@ package com.tiketeer.TiketeerWaiting.domain.waiting.usecase
 import com.tiketeer.TiketeerWaiting.domain.waiting.usecase.dto.GetRankAndTokenCommandDto
 import com.tiketeer.TiketeerWaiting.domain.waiting.usecase.dto.GetRankAndTokenResultDto
 
-class GetRankAndTokenUseCase: GetRankAndToken {
-    override fun getRankAndToken(dto: GetRankAndTokenCommandDto): GetRankAndTokenResultDto {
-        TODO("Not yet implemented")
-    }
-
+interface GetRankAndToken {
+    fun getRankAndToken(dto: GetRankAndTokenCommandDto): GetRankAndTokenResultDto
 }
