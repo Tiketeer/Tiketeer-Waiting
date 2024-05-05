ba487497fb4d5b841b20d63de1d8ef068f5a325b
One-armed-boy <fanatic5500@gmail.com>
2024-05-05 21:33:58 +0900

    JwtAuthenticationManager 작성

--- src/main/kotlin/com/tiketeer/TiketeerWaiting/configuration/SecurityConfig.kt
+++ src/main/kotlin/com/tiketeer/TiketeerWaiting/configuration/SecurityConfig.kt
@@ -1,9 +1,9 @@
 package com.tiketeer.TiketeerWaiting.configuration;
 
+import com.tiketeer.TiketeerWaiting.auth.jwt.JwtAuthenticationManager
 import com.tiketeer.TiketeerWaiting.auth.jwt.JwtServerAuthenticationConverter
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.ReactiveAuthenticationManager
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder
 import org.springframework.security.config.web.server.ServerHttpSecurity
@@ -16,7 +16,7 @@ import org.springframework.security.web.server.authentication.AuthenticationWebF
 @Configuration
 @EnableWebFluxSecurity
 class SecurityConfig(
-		authenticationManager: ReactiveAuthenticationManager,
+		authenticationManager: JwtAuthenticationManager,
 		serverAuthenticationConverter: JwtServerAuthenticationConverter
 ) {
 	private val authenticationWebFilter: AuthenticationWebFilter = AuthenticationWebFilter(authenticationManager)
